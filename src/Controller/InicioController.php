9d97f2812923a2d16551b349954d4a92a1120fb7
Maruve8 <mv.contrerasbellido@gmail.com>
2024-04-15 11:15:21 +0200

    gráfico carga de trabajo con Highcharts

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -5,13 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UsuariosRepository;
 
 class InicioController extends AbstractController
 {
     #[Route('/inicio', name: 'app_inicio')]
-    public function index(): Response
+    public function index(UsuariosRepository $usuariosRepository): Response
     {
+        $usuarios = $usuariosRepository->findAll();
+        $labels = [];
+        $data = [];
+
+        foreach ($usuarios as $usuario) {
+            $labels[] = $usuario->getNombre();
+            $data[] = count($usuario->getTareas());
+        }
+
         return $this->render('inicio/index.html.twig', [
+            'labels' => json_encode($labels),
+            'data' => json_encode($data),
             'controller_name' => 'InicioController',
         ]);
     }
