6ecd91b6ff94c9395bfc5037db33734d06360c6a
Ele Da Fu <d_l_m88@yahoo.es>
2024-04-15 14:25:44 +0200

    restriccion acceso

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -21,39 +21,43 @@ class InicioController extends AbstractController
     #[Route('/inicio', name: 'app_inicio')]
     public function index(UsuariosRepository $usuariosRepository, Request $request): Response
     {
-        $usuarios = $usuariosRepository->findAll();
-        $labels = [];
-        $data = [];
+        if ($this->getUser()) {
+            $usuarios = $usuariosRepository->findAll();
+            $labels = [];
+            $data = [];
 
-        foreach ($usuarios as $usuario) {
-            $labels[] = $usuario->getNombre();
-            $data[] = count($usuario->getTareas());
+            foreach ($usuarios as $usuario) {
+                $labels[] = $usuario->getNombre();
+                $data[] = count($usuario->getTareas());
+            }
+
+            // Obtener todos los clientes desde el repositorio
+            $clientes = $this->clientesRepository->findAll();
+            // Obtener el total de clientes
+            $totalClientes = $this->clientesRepository->count([]);
+
+            // Preparar los datos para el gráfico de clientes
+            $datosClientes = [];
+            foreach ($clientes as $cliente) {
+                // Contar la cantidad de proyectos asociados a cada cliente
+                $cantidadProyectos = count($cliente->getProyectos());
+
+                // Agregar los datos al array
+                $datosClientes[] = [
+                    'nombre' => $cliente->getNombre(),
+                    'cantidad' => $cantidadProyectos,
+                ];
+            }
+
+            return $this->render('inicio/index.html.twig', [
+                'labels' => json_encode($labels),
+                'data' => json_encode($data),
+                'clientes' => $datosClientes,
+                'totalClientes' => $totalClientes,
+            ]);
+        } else {
+            return $this->redirectToRoute('app_login');
         }
-
-        // Obtener todos los clientes desde el repositorio
-        $clientes = $this->clientesRepository->findAll();
-        // Obtener el total de clientes
-        $totalClientes = $this->clientesRepository->count([]);
-
-        // Preparar los datos para el gráfico de clientes
-        $datosClientes = [];
-        foreach ($clientes as $cliente) {
-            // Contar la cantidad de proyectos asociados a cada cliente
-            $cantidadProyectos = count($cliente->getProyectos());
-
-            // Agregar los datos al array
-            $datosClientes[] = [
-                'nombre' => $cliente->getNombre(),
-                'cantidad' => $cantidadProyectos,
-            ];
-        }
-
-        return $this->render('inicio/index.html.twig', [
-            'labels' => json_encode($labels),
-            'data' => json_encode($data),
-            'clientes' => $datosClientes,
-            'totalClientes' => $totalClientes,
-        ]);
     }
 }
 
