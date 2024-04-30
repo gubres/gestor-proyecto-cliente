1020a94dcf5c90c19e53e71bda6b8c1a0a25a5f7
Gustavo Iago <gustavoiago@hotmail.com>
2024-04-30 15:08:25 +0200

    Ajuste del dom en tareas y la variable del if en clientes

--- migrations/Version20240430130104.php
+++ migrations/Version20240430130104.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240429131926 extends AbstractMigration
+final class Version20240430130104 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240429131926 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tareas ADD descripcion VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE tareas ADD finalizado_en DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tareas DROP descripcion');
+        $this->addSql('ALTER TABLE tareas DROP finalizado_en');
     }
 }
